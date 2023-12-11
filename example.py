1f81c88fb7b85d8499acbc4d25775015ee8bee9a
Max Jerdee <mjerdee@umich.edu>
2023-12-11 00:43:48 -0500

    Add files to link to readthedocs

--- example.py
+++ example.py
@@ -1,14 +1,14 @@
 import ranking
 import matplotlib.pyplot as plt
 
-# match_list = ranking.read_match_list_from_gml("data/gml_files/dogs.gml")
+match_list = ranking.read_match_list_from_gml("data/gml_files/dogs.gml")
 # match_list = ranking.read_match_list_from_match_list("data/match_lists/college_football_D1_2023.txt")
-match_list = ranking.read_match_list_from_match_list("data/match_lists/tennis.txt")
+# match_list = ranking.read_match_list_from_match_list("data/match_lists/tennis.txt")
 
 scores = ranking.scores(match_list,model_name='depth_and_luck',force_mode='average')
 
 print(scores)
 
-ranking.draw_depth_and_luck_posterior(match_list)
+# ranking.draw_depth_and_luck_posterior(match_list)
 
-plt.savefig('data/parameter_posteriors/tennis.png')
+# plt.savefig('data/parameter_posteriors/tennis.png')
